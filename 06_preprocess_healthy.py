e28e136f7b9db10f6c99e063f8bcd9b1514ce238
Mathilda Stigenberg <98012841+MathildaStigenberg@users.noreply.github.com>
2023-06-12 13:44:17 +0200

    preprocessed healthy data

---
+++ 06_preprocess_healthy.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+## Preprocess healthy samples
+
+# Import packages
+import scanpy as sc
+from anndata import AnnData
+import anndata as ad
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
+import sys
+
+# Path to concatenated healthy file
+concat_file = sys.argv[1]
+# output file
+output = sys.argv[2]
+
+dat = sc.read_h5ad(concat_file)
+
+dat.var_names_make_unique()
+
+# Mitochondrial genes
+dat.var["mt"] = dat.var_names.str.startswith("MT-")
+# Ribosomal genes
+dat.var["ribo"] = dat.var_names.str.startswith(("RPS", "RPL"))
+# Hemoglobin genes
+dat.var["hb"] = dat.var_names.str.startswith(("^HB[^(P)]"))
+
+# Calculate QC metrics 
+sc.pp.calculate_qc_metrics(
+    dat, qc_vars=["mt", "ribo", "hb"], inplace=True, percent_top=[20], log1p=True
+)
+
+# Function on how to define an outlier 
+def is_outlier(dat, metric: str, nmads: int):
+    M = dat.obs[metric]
+    outlier = (M < np.median(M) - nmads * M.mad()) | (                
+        np.median(M) + nmads * M.mad() < M
+    )
+    return outlier
+
+# Add column to obs which displays if cells are outliers or not based on the covariates. 
+dat.obs["outlier"] = (
+    is_outlier(dat, "log1p_total_counts", 5)
+    | is_outlier(dat, "log1p_n_genes_by_counts", 5)
+    | is_outlier(dat, "pct_counts_in_top_20_genes", 5)
+)
+dat.obs.outlier.value_counts()
+
+# Filter cells based on the mitochondrial content
+dat.obs["mt_outlier"] = is_outlier(dat, "pct_counts_mt", 3) | (
+    dat.obs["pct_counts_mt"] > 8
+)
+dat.obs.mt_outlier.value_counts()
+
+# Remove the outliers 
+dat = dat[(~dat.obs.outlier) & (~dat.obs.mt_outlier)].copy()
+
+# Filter out genes not detected in at least 20 cells
+sc.pp.filter_genes(dat, min_cells=20)
+
+# Filter cells with at most 200 genes
+dat = dat.copy()
+sc.pp.filter_cells(dat, min_genes=200)
+
+# Normalize
+dat = dat.copy()
+dat.layers['normalized'] = dat.X
+dat.X = dat.layers['normalized']
+sc.pp.normalize_total(dat, target_sum=1e6)
+
+# Write to file
+dat.write(output)
